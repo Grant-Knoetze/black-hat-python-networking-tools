bf14f228213165b6a1e37c9544d1a3bb8098bef6
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2021-12-13 17:04:55 +0300

    Add new files

---
+++ cryptor.py
@@ -0,0 +1,6 @@
+from Cryptodome.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from io import BytesIO
+import base64
+import zlib
